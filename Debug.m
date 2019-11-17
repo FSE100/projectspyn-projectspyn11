2848981d62da83e2c2e2458402b372aebc67e966
Jellayy <46771056+Jellayy@users.noreply.github.com>
2019-11-17 13:47:15 -0700

    Week 5 changes
    Added proper turning based on radius which will help in the future. However, for now, everything is just broken. Will fix in autopilot v2

--- Debug.m
+++ Debug.m
@@ -1,10 +1,14 @@
+%Initializing global keyboard control
 global key
 InitKeyboard();
+
+%Initializing color sensor
 brick.SetColorMode(2, 2);
 
+%One big fat driving loop because matlab sucks and I cant do this any other
+%way
 while 1
     pause(0.1);
-    brick.GyroCalibrate(4);
     distance = brick.UltrasonicDist(1);
     disp('ULTRASONIC');
     disp(distance);
@@ -18,30 +22,29 @@ while 1
     switch key
         case 'uparrow'
             disp('UP');
-            brick.MoveMotor('A', -100);
-            brick.MoveMotor('D', -100);
-            
+            brick.MoveMotor('A', -60);
+            brick.MoveMotor('D', -60);
         case 'downarrow'
             disp('DOWN');
-            brick.MoveMotor('A', 100);
-            brick.MoveMotor('D', 100);
+            brick.MoveMotor('A', 60);
+            brick.MoveMotor('D', 60);
             
         case 'leftarrow'
             disp('LEFT');
-            brick.MoveMotor('A', 100);
-            brick.MoveMotor('D', -100);
+            brick.MoveMotor('A', 60);
+            brick.MoveMotor('D', -60);
             
         case 'rightarrow'
             disp('RIGHT');
-            brick.MoveMotor('A', -100);
-            brick.MoveMotor('D', 100);
+            brick.MoveMotor('A', -60);
+            brick.MoveMotor('D', 60);
         case 'w'
             disp('W');
-            brick.MoveMotor('C', -10);
+            brick.MoveMotor('C', -50);
             
         case 's'
             disp('S');
-            brick.MoveMotor('C', 10);
+            brick.MoveMotor('C', 50);
             
         case 0
             disp('Void');
@@ -54,3 +57,6 @@ while 1
     end
 end
 CloseKeyboard();
+
+%This is where I would put my functions, IF MATLAB WASNT STUPID AND GLOBAL
+%VARIABLES ACTUALLY PASSED THROUGH TO FUNCTIONS
--- autopilot.m
+++ autopilot.m
@@ -2,51 +2,94 @@
 global key
 InitKeyboard();
 
+%Initializing color sensor
+brick.SetColorMode(2, 2);
+
 x = 0;
 %Loop to make everything happen more than once
 while x == 0
     pause(.1);
-       switch key
-        case 'space'
-            %This switch is basically only here for when things go horribly
-            %wrong I can mash the spacebar and everything will be ok
-            x = 1;
-            break;
-        case 0
             %The actual program starts here
             
             %Updates color and distance sensors every loop
             distance = brick.UltrasonicDist(1);
+            color = brick.ColorCode(2);
+            
+            %Stop at red
+            if color == 5
+                brick.StopMotor('A');
+                brick.StopMotor('D');
+                pause(5);
+                brick.MoveMotor('A', -60);
+                brick.MoveMotor('D', -60);
+                pause(2);
+            end
+            
+            %pickup at yellow
+            if color == 4
+                %convert to manual input for pickup
+                i = 0;
+                while i == 0
+                    switch key
+                        case 'uparrow'
+                            disp('FORWARD');
+                            brick.MoveMotor('A', -60);
+                            brick.MoveMotor('D', -60);
+                        case 'downarrow'
+                            disp('BACKWARD');
+                            brick.MoveMotor('A', 60);
+                            brick.MoveMotor('D', 60);
+                        case 'leftarrow'
+                            disp('LEFT');
+                            brick.MoveMotor('A', 60);
+                            brick.MoveMotor('D', -60);
+                        case 'rightarrow'
+                            disp('RIGHT');
+                            brick.MoveMotor('A', -60);
+                            brick.MoveMotor('D', 60);
+                        case 'w'
+                            disp('CLAW UP');
+                            brick.MoveMotor('C', -100);
+                        case 's'
+                            disp('CLAW DOWN');
+                            brick.MoveMotor('C', 100);
+                        case 0
+                            disp('STOP');
+                            brick.StopMotor('A');
+                            brick.StopMotor('D');
+                            brick.StopMotor('C');
+                        case 'q'
+                            i = 1;
+                            break;
+                    end
+                end
+            end
             
             %Driving Function
-            speed = 100;
             brick.MoveMotor('A', -60);
             brick.MoveMotor('D', -60);
             %if wall, then turn and pray
-            if distance < 20
+            if distance < 25
                 disp("Wall Detected");
                 brick.StopMotor('A');
                 brick.StopMotor('D');
-                brick.MoveMotor('A', -50);
-                brick.MoveMotor('D', 50);
-                pause(2.1);
+                brick.MoveMotorAngleRel('A', 20, 540, 'Brake');
+                brick.MoveMotorAngleRel('D', -20, 540, 'Brake');
+                brick.WaitForMotor('A');
                 distance = brick.UltrasonicDist(1);
                 if distance < 40
                     disp("Wall Detected");
-                    brick.MoveMotor('A', 50);
-                    brick.MoveMotor('D', -50);
-                    pause(4);
-                    brick.StopMotor('A');
-                    brick.StopMotor('D');
+                    brick.MoveMotorAngleRel('A', -20, 1080, 'Brake');
+                    brick.MoveMotorAngleRel('D', 20, 1080, 'Brake');
+                    brick.WaitForMotor('A');
                     pause(.1);
                     distance = brick.UltrasonicDist(1);
                     %CURRENTLY INCOMPLETE. For getting around tricky parts
                     %of the map. very wip.
                     if distance < 40
                         disp("Dead End Detected");
-                        brick.MoveMotor('A', 50);
-                        brick.MoveMotor('D', -50);
-                        pause(2.1);
+                        brick.MoveMotorAngleRel('A', 20, 540, 'Brake');
+                        brick.MoveMotorAngleRel('D', -20, 540, 'Brake');
                         brick.MoveMotor('A', -60);
                         brick.MoveMotor('D', -60);
                         pause(4);
@@ -54,12 +97,24 @@ while x == 0
                         brick.StopMotor('D');
                         brick.MoveMotor('A', 50);
                         brick.MoveMotor('D', -50);
-                        pause(2.1);
+                        pause(1);
                     end
                         
                 end
             end
-    end
+            
+            %Dropoff at blue
+            if color == 2
+              brick.StopMotor('A');
+              brick.StopMotor('D');  
+              brick.MoveMotor('C', 100);
+              pause(2);
+              brick.MoveMotor('A', 60);
+              brick.MoveMotor('D', 60);
+              pause(2);
+              x = 1;
+              break;
+            end
 end
 
 CloseKeyboard();
